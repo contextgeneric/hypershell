8c2404a697a05fb030f59066a6333ed672c57fb9
Soares Chen <soares.chen@maybevoid.com>
2025-05-26 12:51:17 +0000

    Improve error handling in RunSimpleExec

--- crates/hypershell-tokio-components/src/providers/simple_exec.rs
+++ crates/hypershell-tokio-components/src/providers/simple_exec.rs
@@ -10,11 +10,6 @@ use tokio::process::Child;
 
 use crate::dsl::CoreExec;
 
-pub struct SimpleExecFailure<'a, Context> {
-    pub context: &'a Context,
-    pub error: std::io::Error,
-}
-
 pub struct ExecOutputError<'a, Context> {
     pub context: &'a Context,
     pub output: Output,
@@ -26,7 +21,9 @@ impl<Context, CommandPath, Args> Handler<Context, SimpleExec<CommandPath, Args>,
 where
     Context: CanHandle<CoreExec<CommandPath, Args>, (), Output = Child>
         + for<'a> CanRaiseAsyncError<ExecOutputError<'a, Context>>
-        + for<'a> CanRaiseAsyncError<SimpleExecFailure<'a, Context>>,
+        + CanWrapAsyncError<StdinPipeError>
+        + CanWrapAsyncError<WaitWithOutputError>
+        + CanRaiseAsyncError<std::io::Error>,
     CommandPath: Send,
     Args: Send,
 {
@@ -39,17 +36,21 @@ where
     ) -> Result<Vec<u8>, Context::Error> {
         let mut child = context.handle(PhantomData, ()).await?;
 
-        if let Some(mut stdin) = child.stdin.take() {
-            stdin
-                .write_all(&input)
-                .await
-                .map_err(|error| Context::raise_error(SimpleExecFailure { context, error }))?;
+        if !input.is_empty() {
+            if let Some(mut stdin) = child.stdin.take() {
+                stdin
+                    .write_all(&input)
+                    .await
+                    .map_err(Context::raise_error)
+                    .map_err(|e| Context::wrap_error(e, StdinPipeError))?;
+            }
         }
 
         let output = child
             .wait_with_output()
             .await
-            .map_err(|error| Context::raise_error(SimpleExecFailure { context, error }))?;
+            .map_err(Context::raise_error)
+            .map_err(|e| Context::wrap_error(e, WaitWithOutputError))?;
 
         if output.status.success() {
             Ok(output.stdout)
@@ -59,9 +60,19 @@ where
     }
 }
 
-impl<'a, Context> Debug for SimpleExecFailure<'a, Context> {
+pub struct StdinPipeError;
+
+impl Debug for StdinPipeError {
     fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
-        write!(f, "error executing command: {}", self.error)
+        write!(f, "error piping input to stdin of child process")
+    }
+}
+
+pub struct WaitWithOutputError;
+
+impl Debug for WaitWithOutputError {
+    fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+        write!(f, "error waiting for output from child process")
     }
 }
 
