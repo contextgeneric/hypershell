fb9832e67918c2073fba1fcf354f3fd64e739e88
Soares Chen <soares.chen@maybevoid.com>
2025-05-27 13:33:42 +0000

    Implement UpdateRequestHeaders

--- crates/hypershell-reqwest-components/src/providers/headers.rs
+++ crates/hypershell-reqwest-components/src/providers/headers.rs
@@ -2,17 +2,18 @@ use core::marker::PhantomData;
 
 use cgp::prelude::*;
 use hypershell_components::components::CanExtractStringArg;
-use hypershell_components::dsl::Header;
+use hypershell_components::dsl::{Header, WithHeaders};
 use reqwest::RequestBuilder;
 use reqwest::header::{HeaderName, HeaderValue, InvalidHeaderName, InvalidHeaderValue};
 
-use crate::components::{RequestBuilderUpdater, RequestBuilderUpdaterComponent};
+use crate::components::{
+    CanUpdateRequestBuilder, RequestBuilderUpdater, RequestBuilderUpdaterComponent,
+};
 
 #[cgp_new_provider]
 impl<Context, Key, Value> RequestBuilderUpdater<Context, Header<Key, Value>> for UpdateRequestHeader
 where
-    Context: HasErrorType
-        + CanExtractStringArg<Key>
+    Context: CanExtractStringArg<Key>
         + CanExtractStringArg<Value>
         + CanRaiseError<InvalidHeaderName>
         + CanRaiseError<InvalidHeaderValue>,
@@ -30,5 +31,41 @@ where
         let value = HeaderValue::from_bytes(value_str.as_bytes()).map_err(Context::raise_error)?;
 
         Ok(builder.header(key, value))
+    }
+}
+
+pub struct UpdateRequestHeaders;
+
+#[cgp_provider]
+impl<Context, Arg, Args> RequestBuilderUpdater<Context, WithHeaders<Cons<Arg, Args>>>
+    for UpdateRequestHeaders
+where
+    Context: CanUpdateRequestBuilder<Arg>,
+    Self: RequestBuilderUpdater<Context, WithHeaders<Args>>,
+{
+    fn update_request_builder(
+        context: &Context,
+        _phantom: PhantomData<WithHeaders<Cons<Arg, Args>>>,
+        builder: RequestBuilder,
+    ) -> Result<RequestBuilder, Context::Error> {
+        let builder = context.update_request_builder(PhantomData, builder)?;
+        let builder =
+            Self::update_request_builder(context, PhantomData::<WithHeaders<Args>>, builder)?;
+
+        Ok(builder)
+    }
+}
+
+#[cgp_provider]
+impl<Context> RequestBuilderUpdater<Context, WithHeaders<Nil>> for UpdateRequestHeaders
+where
+    Context: HasErrorType,
+{
+    fn update_request_builder(
+        _context: &Context,
+        _phantom: PhantomData<WithHeaders<Nil>>,
+        builder: RequestBuilder,
+    ) -> Result<RequestBuilder, Context::Error> {
+        Ok(builder)
     }
 }
